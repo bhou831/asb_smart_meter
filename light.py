160360adff2d83262a5a7d3bc83df63d1d1914c0
bhou831 <bowenhou@andrew.cmu.edu>
2023-04-02 19:37:20 -0400

    test

--- light.py
+++ light.py
@@ -1,1 +1,33 @@
-ss
+import RPi.GPIO as GPIO
+import time
+import sys
+import csv
+
+button = 25
+
+# Set parameters for GPIO
+
+GPIO.setwarnings(False)
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(button, GPIO.IN)
+
+filename = "buttonpresses.csv"
+with open(filename, 'a') as file:
+    fd = csv.writer(file)
+    w.writerow("Timestamp, ButtonValue")
+
+    
+
+def main():
+    while True:
+        GPIO.output(red_led, GPIO.HIGH)
+        time.sleep(3)
+        GPIO.output(red_led, GPIO.LOW)
+        time.sleep(1)
+
+if __name__ == '__main__':
+    try:
+        main()
+    except KeyboardInterrupt:
+        GPIO.cleanup()
+        sys.exit(0)
---
+++ red_light.py
@@ -0,0 +1,14 @@
+import RPi.GPIO as GPIO
+import time
+
+GPIO.setmode(GPIO.BCM)
+
+red_led = 18
+
+GPIO.setup(red_led, GPIO.OUT)
+
+while True:
+    GPIO.output(red_led, GPIO.HIGH)
+    time.sleep(3)
+    GPIO.output(red_led, GPIO.LOW)
+    time.sleep(1)
