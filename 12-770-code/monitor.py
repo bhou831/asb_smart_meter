0ebe18b4088150ed59a45052b4d99a6f265df674
bhou831 <bowenhou@andrew.cmu.edu>
2023-04-16 13:59:56 -0400

    update monitor function

--- 12-770-code/monitor.py
+++ 12-770-code/monitor.py
@@ -27,6 +27,7 @@ FILE_PATH = "energy_data_4.csv"
 fig = make_subplots(rows=1, cols=1, specs=[[{'type': 'scatter'}]])
 trace_voltage = go.Scatter(x=[], y=[], mode='lines+markers', name='Voltage')
 fig.add_trace(trace_voltage)
+start_time = time.time()
 
 # Define an update function for the plot
 def update_plot(x_data, y_data):
@@ -35,37 +36,27 @@ def update_plot(x_data, y_data):
 
 
 
-    for i in range(60):
-        spi_bus = busio.SPI(board.SCK, MISO=board.MISO, MOSI=board.MOSI)
-        cs = digitalio.DigitalInOut(board.D5)
-        energy_sensor = ATM90e32(spi_bus, cs, lineFreq, PGAGain,
-                         VoltageGain, CurrentGainCT1, 0, CurrentGainCT2)# Collect data for 60 seconds# Open the CSV file for writing
-        # Read the energy data from the sensor
-        sys0 = energy_sensor.sys_status0
-        voltageA = energy_sensor.line_voltageA
-        voltageC = energy_sensor.line_voltageC
-        if (lineFreq == 4485):  # split single phase
-            totalVoltage = voltageA + voltageC
-        else:
-            totalVoltage = voltageA  # 220-240v
-        
-        # Write the energy data to the CSV file
-        writer.writerow({'time': i, 
-                         'voltage1': voltageA*120/640, 
-                         'voltage2': voltageC*120/640, 
-                         'current1': energy_sensor.line_currentA*5/0.1, 
-                         'current2': energy_sensor.line_currentC*5/0.1, 
-                         'frequency': energy_sensor.frequency*60/50, 
-                         'active_power': energy_sensor.active_power})
-        
-        x_data = [time.time() - start_time] # append current time to list
-        y_data = [voltageA*120/640] # append current voltage to list
+for i in range(60):
+    spi_bus = busio.SPI(board.SCK, MISO=board.MISO, MOSI=board.MOSI)
+    cs = digitalio.DigitalInOut(board.D5)
+    energy_sensor = ATM90e32(spi_bus, cs, lineFreq, PGAGain,
+                        VoltageGain, CurrentGainCT1, 0, CurrentGainCT2)# Collect data for 60 seconds# Open the CSV file for writing
+    # Read the energy data from the sensor
+    sys0 = energy_sensor.sys_status0
+    voltageA = energy_sensor.line_voltageA
+    voltageC = energy_sensor.line_voltageC
+    if (lineFreq == 4485):  # split single phase
+        totalVoltage = voltageA + voltageC
+    else:
+        totalVoltage = voltageA  # 220-240v
+    
+    x_data = [time.time() - start_time] # append current time to list
+    y_data = [voltageA*120/640] # append current voltage to list
 
-        # Update the live plot
-        update_plot(x_data, y_data)
+    # Update the live plot
+    update_plot(x_data, y_data)
 
-        time.sleep(1)
+    time.sleep(1)
         
 # Print a message to indicate that the CSV file has been written
 print(f"Energy data saved to {FILE_PATH}")
-root.mainloop()
