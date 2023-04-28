f9c0e982b24a6030925d6f189153e19f35e4bd8f
bhou831 <bowenhou@andrew.cmu.edu>
2023-04-28 00:14:48 -0400

    test

--- run.sh
+++ run.sh
@@ -2,7 +2,7 @@
 
 # This script will run read-value.py, terminate it after 50 minutes, and repeat this process 10 times.
 # Loop 10 times
-for i in {1..5}; do
+for i in {1..15}; do
   echo "Starting iteration $i of read-values.py"
   
   # Run read-value.py in the background and save its process ID (PID)
@@ -10,13 +10,14 @@ for i in {1..5}; do
   read_value_pid=$!
   
   # Wait for 50 minutes (3000 seconds)
-  sleep 31s
+  sleep 20s
   
   # Terminate the read-value.py process using its PID
   echo "Terminating iteration $i of read-value.py"
+  kill -2 $read_value_pid
 
   # Wait a bit before starting the next iteration
-  sleep 2s
+  sleep 1s
 done
 
 echo "All 10 iterations completed."
