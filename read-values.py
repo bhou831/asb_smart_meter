c6282f1559522710c9ecb76e6ab16513d8829788
bhou831 <bowenhou@andrew.cmu.edu>
2023-04-27 21:47:59 -0400

    update function with a wrapper

--- read-values.py
+++ read-values.py
@@ -4,11 +4,11 @@ import busio
 import time
 import csv
 from atm90e32 import ATM90e32
-from adafruit_bus_device.spi_device import SPIDevice
-import matplotlib.pyplot as plt
 import gc
 import datetime
 import json
+import sys
+import traceback
 
 with open('config.json') as file:
     configuration = json.load(file)
@@ -47,40 +47,57 @@ def deinit_resources(spi_bus, cs):
     cs.deinit()
     spi_bus.deinit()
 
-with open(FILE_PATH, mode='w') as csv_file:
-    fieldnames = ['time', 'voltage', 'current', 'frequency', 'power']
-    writer = csv.DictWriter(csv_file, fieldnames=fieldnames)
-    
-    # Write the header row to the CSV file
-    writer.writeheader()
-
-    # Loop for 60 seconds
-    for i in range(OBSERVATION_TIME):
-        spi_bus, cs, energy_sensor = init_energy_sensor()
-
-        # Read the energy data from the sensor
-        sys0 = energy_sensor.sys_status0
-        voltage = energy_sensor.line_voltageA
-        current = energy_sensor.line_currentA
+def write_to_csv():
+    with open(FILE_PATH, mode='w') as csv_file:
+        fieldnames = ['time', 'voltage', 'current', 'frequency', 'power']
+        writer = csv.DictWriter(csv_file, fieldnames=fieldnames)
         
-        # filter out the irregular current spikes
-        if len(voltage_lst) > 0 and (current > 4 or voltage < 70):
-            current = current_lst[-1]
-            voltage = voltage_lst[-1]
-        
-        voltage_lst.append(voltage)
-        current_lst.append(current)
+        # Write the header row to the CSV file
+        writer.writeheader()
 
-        # Write the energy data to the CSV file
-        writer.writerow({'time': i, 
-                         'voltage': voltage*120/640,
-                         'current': current,
-                         'frequency': energy_sensor.frequency*60/50,
-                         'power': voltage*120/640*current})
-        
-        deinit_resources(spi_bus, cs)
-        # del energy_sensor
-        # gc.collect()
-        time.sleep(MEASUREMENT_GRANULARITY)
-# Print a message to indicate that the CSV file has been written
-print(f"Energy data saved to {FILE_PATH}")
+        # Loop for 60 seconds
+        for i in range(OBSERVATION_TIME):
+            spi_bus, cs, energy_sensor = init_energy_sensor()
+
+            # Read the energy data from the sensor
+            sys0 = energy_sensor.sys_status0
+            voltage = energy_sensor.line_voltageA
+            current = energy_sensor.line_currentA
+            
+            # filter out the irregular current spikes
+            if len(voltage_lst) > 0 and (current > 4 or voltage < 70):
+                current = current_lst[-1]
+                voltage = voltage_lst[-1]
+            
+            voltage_lst.append(voltage)
+            current_lst.append(current)
+
+            # Write the energy data to the CSV file
+            writer.writerow({'time': i, 
+                            'voltage': voltage*120/640,
+                            'current': current,
+                            'frequency': energy_sensor.frequency*60/50,
+                            'power': voltage*120/640*current})
+            
+            deinit_resources(spi_bus, cs)
+            del energy_sensor
+            gc.collect()
+            time.sleep(MEASUREMENT_GRANULARITY)
+    # Print a message to indicate that the CSV file has been written
+    print(f"Energy data saved to {FILE_PATH}")
+
+# Retry the function if OSError is encountered
+def write_to_csv_wrapper(retry_count=10):
+    try:
+        write_to_csv()
+    except OSError as e:
+        if e.errno == 24 and retry_count > 0:  # Error 24: Too many open files
+            print(f"OSError encountered: {e}. Retrying...")
+            time.sleep(1)  # Give some time for the system to close open files
+            write_to_csv_wrapper(retry_count=retry_count - 1)  # Retry the function with reduced retry count
+        else:
+            print(f"Error occurred: {e}", file=sys.stderr)
+            traceback.print_exc()
+
+if __name__ == "__main__":
+    write_to_csv_wrapper()
