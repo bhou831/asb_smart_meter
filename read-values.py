8348a6deac5ffb47384e69ea8a99f263ce7d1682
bhou831 <bowenhou@andrew.cmu.edu>
2023-04-28 12:34:57 -0400

    new read-value program

--- read-values.py
+++ read-values.py
@@ -7,6 +7,7 @@ from atm90e32 import ATM90e32
 import gc
 import datetime
 import json
+import os
 
 with open('config.json') as file:
     configuration = json.load(file)
@@ -26,11 +27,7 @@ CurrentGainCT1 = 38695  # 38695 - SCT-016 120A/40mA
 CurrentGainCT2 = 25498  # 25498 - SCT-013-000 100A/50mA
 # 46539 - Magnalab 100A w/ built in burden resistor
 
-# adjust the time threshold to collect data
-OBSERVATION_TIME = configuration["OBSERVATION_TIME"] 
-MEASUREMENT_GRANULARITY = configuration["MEASUREMENT_GRANULARITY"]
-
-FILE_PATH = f"data/energy_data_{timestamp}.csv"
+FILE_PATH = f"data/energy_data.csv"
 
 current_lst = []
 voltage_lst = []
@@ -46,41 +43,43 @@ def deinit_resources(spi_bus, cs):
     spi_bus.deinit()
 
 def write_to_csv():
-    with open(FILE_PATH, mode='w') as csv_file:
+    # Check if the file exists and has content
+    file_exists = os.path.isfile(FILE_PATH) and os.path.getsize(FILE_PATH) > 0
+
+    with open(FILE_PATH, mode='a') as csv_file:
         fieldnames = ['time', 'voltage', 'current', 'frequency', 'power']
         writer = csv.DictWriter(csv_file, fieldnames=fieldnames)
-        
-        # Write the header row to the CSV file
-        writer.writeheader()
 
-        # Loop for 60 seconds
-        for i in range(OBSERVATION_TIME):
-            spi_bus, cs, energy_sensor = init_energy_sensor()
+        # Write the header row to the CSV file if it doesn't exist
+        if not file_exists:
+            writer.writeheader()
 
-            # Read the energy data from the sensor
-            sys0 = energy_sensor.sys_status0
-            voltage = energy_sensor.line_voltageA
-            current = energy_sensor.line_currentA
-            
-            # filter out the irregular current spikes
-            if len(voltage_lst) > 0 and (current > 4 or voltage < 70):
-                current = current_lst[-1]
-                voltage = voltage_lst[-1]
-            
-            voltage_lst.append(voltage)
-            current_lst.append(current)
+        spi_bus, cs, energy_sensor = init_energy_sensor()
 
-            # Write the energy data to the CSV file
-            writer.writerow({'time': int(time.time()), 
-                            'voltage': voltage*120/640,
-                            'current': current,
-                            'frequency': energy_sensor.frequency*60/50,
-                            'power': voltage*120/640*current})
-            csv_file.flush()
-            deinit_resources(spi_bus, cs)
-            del energy_sensor
-            gc.collect()
-            time.sleep(MEASUREMENT_GRANULARITY)
+        # Read the energy data from the sensor
+        sys0 = energy_sensor.sys_status0
+        voltage = energy_sensor.line_voltageA
+        current = energy_sensor.line_currentA
+
+        # filter out the irregular current spikes
+        if len(voltage_lst) > 0 and (current > 4 or voltage < 70):
+            current = current_lst[-1]
+            voltage = voltage_lst[-1]
+
+        voltage_lst.append(voltage)
+        current_lst.append(current)
+
+        # Write the energy data to the CSV file
+        writer.writerow({'time': int(time.time()),
+                         'voltage': voltage * 120 / 640,
+                         'current': current,
+                         'frequency': energy_sensor.frequency * 60 / 50,
+                         'power': voltage * 120 / 640 * current})
+        csv_file.flush()
+        deinit_resources(spi_bus, cs)
+        del energy_sensor
+        gc.collect()
+
     # Print a message to indicate that the CSV file has been written
     print(f"Energy data saved to {FILE_PATH}")
 
